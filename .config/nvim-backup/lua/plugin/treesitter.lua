ed7797668f112aa2eed7ac57d9339bcb24e87058
Cameron Rasico <53454804+CRasico@users.noreply.github.com>
2023-01-03 14:21:06 -0500

    dap and magic (#30)

--- .config/nvim-backup/lua/plugin/treesitter.lua
+++
@@ -1,7 +0,0 @@
-require('nvim-treesitter.configs').setup({
-	ensure_installed = { "c", "lua", "rust", "c_sharp", "python", "typescript" },
-	auto_install = true,
-	highlight = {
-		enable = true
-	}
-})

12ffcf2df5a59ca405a29957b42338c3bc846155
Cameron Rasico <rasicoca@gmail.com>
2023-12-04 21:54:40 -0500

    some additional work with lazy

--- .config/nvim/lua/crasico/plugins/mason.lua
+++ .config/nvim/lua/crasico/plugins/mason.lua
@@ -3,11 +3,13 @@ return {
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"neovim/nvim-lspconfig"
 	},
 	config = function()
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
 		local mason_tool_installer = require("mason-tool-installer")
+		local lsp_zero = require("lsp-zero")
 
 		mason.setup({
 			ui = {
@@ -29,6 +31,9 @@ return {
 				"lua_ls",
 			},
 			automatic_installation = true,
+			handlers = {
+				lsp_zero.default_setup,
+			}
 		})
 
 		mason_tool_installer.setup({

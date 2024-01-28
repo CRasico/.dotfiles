1a1c7bc39f189e8a01b8e90ec4e3f143ae5c2c92
Cameron Rasico <rasicoca@gmail.com>
2024-01-27 23:39:31 -0500

    getting closer

--- .config/nvim/lua/crasico/plugins/mason.lua
+++ .config/nvim/lua/crasico/plugins/mason.lua
@@ -2,24 +2,48 @@ return {
   "williamboman/mason.nvim",
 	dependencies = {
     "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
   config = function()
     local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
 		mason.setup()
 
 		mason_lspconfig.setup({
       ensure_installed = {
-        "tsserver",
-				"cssls",
-				"lua_ls",
-				"csharp_ls",
-				"gopls",
-				"rust_analyzer",
+        "tsserver", -- ts language server
+				"html", -- html language server
+				"cssls", -- css language server
+				"lua_ls", -- lua language server
+				"csharp_ls", -- c# language server
+				"gopls", -- go language server
+				"rust_analyzer", -- rust language server
+			},
+			handlers = {
+				csharp_ls = function()
+				  local root_dir = function(startpath)
+					  return require("lspconfig").util.root_pattern("*.sln")(startpath)
+							or require("lspconfig").util.root_pattern("*.csproj")(startpath)
+							or require("lspconfig").util.root_pattern("*.fsproj")(startpath)
+							or require("lspconfig").util.root_pattern("*.git")(startpath)
+				  end
+				  return {
+					  root_dir = root_dir
+				  }
+			  end
 			}
 		})
+
+		mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+				"csharpier", -- c# formatter
+        "eslint_d", -- js linter
+      },
+    })
 
 		local keymap = vim.keymap
 
